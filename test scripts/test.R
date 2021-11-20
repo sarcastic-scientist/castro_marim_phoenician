9e9fff811d31b72255999bd8e70ff26c9a0de720
sarcastic-scientist <roshanp256121@gmail.com>
2021-11-20 23:00:43 +0100

    1) Coastal map added 2) Anne France corrections added. 3) Geological map corrected.

--- test scripts/test.R
+++ test scripts/test.R
@@ -6,13 +6,15 @@ library(knitr)
 library(patchwork)
 library(ggbeeswarm)
 library(RColorBrewer)
+library(stringr)
+library(knitr)
 
 here::here()
 
-df_iso_total <- here::here("./data/castro_iso_total_1.csv") %>%
+df_iso_total <- here::here("./data/castro_iso_total.csv") %>%
   read_csv(col_names = TRUE)
 
-df_iso_fauna_summ <- here::here("./data/castro_fauna_iso_summ_1.csv") %>%
+df_iso_fauna_summ <- here::here("./data/castro_fauna_iso_summ.csv") %>%
   readr::read_csv(col_names = TRUE)
 
 theme<-theme(panel.background = element_blank(),legend.background = element_blank(), legend.key.size = unit(0.3, 'cm'), legend.box.background = element_rect(colour = "black"), legend.position="right",legend.text = element_text(size=6),text = element_text(),legend.title=element_text(size=7),axis.title.x = element_text(size=8), axis.title.y = element_text(size=8), panel.border=element_rect(fill=NA),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),strip.background=element_blank(), axis.text.x=element_text(colour="black"),axis.text.y=element_text(colour="black"),axis.ticks=element_line(colour="black"),plot.margin=unit(c(1,1,1,1),"line"))
@@ -61,4 +63,19 @@ iso_sulphur_plot <- ggplot(dffinal_iso_summ, aes(x=Common_Name,y=mean_dS, color=
   geom_point(size = 3, alpha = 0.65) + geom_errorbar(aes(ymin=mean_dS-std.dev_dS, ymax=mean_dS+std.dev_dS), width=.1) + theme + labs( x='',y=expression(paste(delta^{34}, "S (\u2030 vs. VCDT)")), shape = 'Species', colour = 'Species') + scale_color_manual(values = c("#b452a2","#5866ad","#4e052e","#25738b","#071576","#00344d","#e30293","#637f4b","#158a2c","#0b4512")) + scale_shape_manual(values = c(15,17,16,15,15,15,17,16,16,17)) + geom_jitter(data = df_iso_fauna, aes(x = Common_Name, y = dS), width=0.25) + scale_x_discrete(limits = c("Grey Plover","Kittiwake","Partridge","Chicken","Pig","Cattle","Goat","Sheep","Rabbit","Red Deer"))
 
 ggsave('sulphfauna.jpg',iso_sulphur_plot)
+
+
+
+options(knitr.kable.NA = "--")
+
+df_iso_bota <- here::here("./data/castro_iso_bota.csv") |>
+  readr::read_csv(col_names = TRUE) |>
+  mutate(Species2=paste0("_", Species, "_")) |>
+  mutate(Species2=str_replace_all(Species," var. ", "_ var. _"),
+         Species2=str_replace_all(Species," subsp. ", "_ subsp. _")) %>%
+  select(ID,Species,percentC,percentN,dC,dN,dC_corr,dN_corr,DC) %>%
+  kable(caption = "(ref:isobot-caption)", align="lcrrrrlcr", booktabs = T, table.env='table*', , digits = 1, linesep = "", format = "latex", escape = FALSE, col.names=c("Sample ID", "Species", "\\% C", "\\% N", "$\\delta^{13}C (\\text{\\textperthousand})$", "$\\delta^{15}N (\\text{\\textperthousand})$", "$\\delta^{13}C_{cr}^{*} (\\text{\\textperthousand})$", "$\\delta^{15}N_{cr}^{*} (\\text{\\textperthousand})$", "$\\Delta^{13}C (\\text{\\textperthousand})$")) %>%
+  kable_styling(latex_options = c("basic","hold_position"), font_size = 7.5, full_width = TRUE) %>%
+  column_spec(2,italic = TRUE)
+
 
